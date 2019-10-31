b5153038388a6afb9f08134471f720775f238581
Aramosgr <aramosgr@gmail.com>
2019-10-31 20:54:24 +0000

    Basic squeleton
    Burguer builder added

--- BurgerApp/src/components/Layout/Layout.js
+++ BurgerApp/src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import Auxiliar from '../../hoc/Auxiliar';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
     <Auxiliar>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Auxiliar>
